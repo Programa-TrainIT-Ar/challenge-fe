66dfbc7730daf682031946e879cf2cb2cb3e4a25
Leonel A.T. Goncalves <liongoncalves@gmail.com>
2024-05-31 19:38:57 +0200

    Removed unused deps and updated eslint deps

--- src/app/demo/components/dashboard/dashboard.component.ts
+++ src/app/demo/components/dashboard/dashboard.component.ts
@@ -3,7 +3,6 @@ import {Observable} from 'rxjs';
 import {UserService} from './UserService';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
-import {ChartModule} from 'primeng/chart';
 import {MenuModule} from 'primeng/menu';
 import {TableModule} from 'primeng/table';
 import {StyleClassModule} from 'primeng/styleclass';
@@ -13,7 +12,7 @@ import {ButtonModule} from 'primeng/button';
 @Component({
     templateUrl: './dashboard.component.html',
     standalone: true,
-    imports: [CommonModule, FormsModule, ChartModule, MenuModule, TableModule, StyleClassModule, PanelMenuModule, ButtonModule,],
+    imports: [CommonModule, FormsModule, MenuModule, TableModule, StyleClassModule, PanelMenuModule, ButtonModule,],
 })
 export class DashboardComponent implements OnInit {
     userService = inject(UserService);
