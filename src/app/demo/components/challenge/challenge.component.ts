541a731d4dc70cc3811500b033cc574eb23c918d
CarlosMrtnzMrtnz <carlosmartinezmartinez2015@gmail.com>
2024-07-17 12:53:25 -0500

    Issue #3 Registro de Participante Registro de usuario una vez se autentica en auth0

---
+++ src/app/demo/components/challenge/challenge.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-challenge',
+  standalone: true,
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './challenge.component.html',
+  styleUrl: './challenge.component.scss'
+})
+export class ChallengeComponent {
+
+}
--- src/app/demo/components/dashboard/dashboard.component.ts
+++ src/app/demo/components/dashboard/dashboard.component.ts
@@ -11,44 +11,133 @@ import {ButtonModule} from 'primeng/button';
 import { AuthService } from '@auth0/auth0-angular';
 import { NestjsApiService } from 'src/app/layout/service/nestjs-api.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { ChallengeComponent } from '../challenge/challenge.component';
+import { Header } from 'primeng/api';
+import { tick } from '@angular/core/testing';
+import { Token } from '@angular/compiler';
+import Swal from 'sweetalert2';
 
 @Component({
     templateUrl: './dashboard.component.html',
     standalone: true,
-    imports: [CommonModule, FormsModule, MenuModule, TableModule, StyleClassModule, PanelMenuModule, ButtonModule,],
+    imports: [CommonModule, FormsModule, MenuModule, TableModule, StyleClassModule, PanelMenuModule, ButtonModule,ChallengeComponent],
 })
 export class DashboardComponent implements OnInit {
+    // [x: string]: import("@auth0/auth0-angular").User;
     userService = inject(UserService);
     nestjsService = inject(NestjsApiService)
-    user$: Observable<unknown>;
-    data = signal<any>([])
+    headers = new HttpHeaders
+    user: import("@auth0/auth0-angular").User;
     constructor(public auth:AuthService, private http: HttpClient){}
 
 
-        ngOnInit(): void {}
+        // ngOnInit(): void {
+        //     this.auth.isAuthenticated$.subscribe(isAuthenticated=>{
+        //         if (isAuthenticated) {
+        //             this.auth.getAccessTokenSilently().subscribe(
+        //                 token => {
+        //                   const headers = new HttpHeaders ({
+        //                     Authorization: `Bearer ${token}`
+        //                   });
+        //                   this.nestjsService.portUser(headers,this.user,).subscribe(
+        //                     (response) => {
+        //                       console.log('Respuesta del endpoint protegido:', response);
+        //                       console.log(token);
 
-        callProtectedEndpoint(): void {
-            if (this.auth.isAuthenticated$) {
-                this.auth.getAccessTokenSilently().subscribe(
-                  token => {
-                    const headers = new HttpHeaders ({
-                      Authorization: `Bearer ${token}`
-                    });
-                    this.nestjsService.getNestjs(headers).subscribe(
-                      (response) => {
-                        console.log('Respuesta del endpoint protegido:', response);
-                      },
-                      (error) => {
-                        console.error('Error al llamar al endpoint protegido:', error);
-                      }
-                    );
-                  },
-                  error => {
-                    console.error('Error obteniendo el access token:', error);
-                  }
-                );
-              }
-            }
+        //                     },
+        //                     (error) => {
+        //                       console.error('Error al llamar al endpoint protegido:', error);
+        //                     }
+        //                   );
+        //                 },
+        //                 error => {
+        //                   console.error('Error obteniendo el access token:', error);
+        //                 }
+        //               );
+
+        //         }
+        //     })
+        // }
+
+        ngOnInit() {
+            this.auth.isAuthenticated$.subscribe(isAuth => {
+                if (isAuth) {
+                    this.userRegister()
+                }
+            })
+        }
+
+        async userRegister() {
+
+            await this.auth.user$.subscribe({
+                next: data => {
+                this.user = {
+                    "name" : data.name,
+                    "nickname" : data.nickname,
+                    "email" : data.email,
+                    "picture" : data.picture
+                }
+                this.auth.getAccessTokenSilently().subscribe({
+                    next: token => {
+                        this.headers = new HttpHeaders ({
+                            Authorization: `Bearer ${token}`
+                        })
+
+                    this.nestjsService.portUser(this.user, this.headers).subscribe({
+                        next : res => {
+                        return Swal.fire({
+                            title: '¡Registro Exitoso!',
+                            text: `Bienvenido a TrainIT ${this.user.name}`,
+                            icon: 'success',
+                            confirmButtonText: 'Aceptar'
+                        })
+                    } , error: (error => {
+                        return console.log("Error al registrar el usuario: ", error.error.message);
+
+                    })
+                })
+                    }, error: (error => {
+                        return console.log("Error al obtener token", error);
+
+                    })
+                })
+
+            }, error: (error =>{
+                return console.log("Error al obtener inf del usuario", error);
+            })
+        })
+
+
+        }
+
+        // callProtectedEndpoint(): void {
+        //     this.auth.isAuthenticated$.subscribe(isAuthenticated  => {
+        //         if (isAuthenticated) {
+        //             this.auth.getAccessTokenSilently().subscribe(
+        //               token => {
+        //                 const headers = new HttpHeaders ({
+        //                   Authorization: `Bearer ${token}`
+        //                 });
+
+        //                 this.nestjsService.getNestjs(headers).subscribe(
+        //                   (response) => {
+        //                     console.log('Respuesta del endpoint protegido:', response);
+        //                     console.log(token);
+
+        //                   },
+        //                   (error) => {
+        //                     console.error('Error al llamar al endpoint protegido:', error);
+        //                   }
+        //                 );
+        //               },
+        //               error => {
+        //                 console.error('Error obteniendo el access token:', error);
+        //               }
+        //             );
+        //           }
+        //     })
+
+        // }
 
 
 
