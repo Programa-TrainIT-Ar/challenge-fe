f7f927d3c24d352211d2b17b20d6417d49325754
matia <matiasminig@gmail.com>
2024-09-16 21:45:22 -0300

    actualizando components

--- src/app/modules/welcome/pages/welcome-page/welcome-page.component.ts
+++ src/app/modules/welcome/pages/welcome-page/welcome-page.component.ts
@@ -2,20 +2,21 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-welcome-page',
-  templateUrl: './welcome-page.component.html',
-  styleUrl: './welcome-page.component.scss',
+    selector: 'app-welcome-page',
+    templateUrl: './welcome-page.component.html',
+    styleUrl: './welcome-page.component.scss',
 })
 export class WelcomePageComponent {
-  constructor(private router: Router) {}
+    constructor(private router: Router) { }
 
-  goToAll() {
-    this.router.navigate(['/all']);
-  }
-  goToCreate() {
-    this.router.navigate(['/create']);
-  }
-  goToEdit() {
-    this.router.navigate(['/edit']);
-  }
+    goToAll() {
+        this.router.navigate(['/all']);
+    }
+    goToCreate() {
+        this.router.navigate(['/create']);
+    }
+    goToEdit() {
+        this.router.navigate(['/edit']);
+    }
+
 }
