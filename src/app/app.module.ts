a999171c3819b9af2c76352a20425b1b48dbf575
ignacio salazar <ignaciosalazar986@gmail.com>
2024-09-17 16:37:08 -0300

    empezando animacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppLayoutModule } from './layout/app.layout.module';
 import { AuthService } from '@auth0/auth0-angular';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AuthModule } from '@auth0/auth0-angular';
 import { environment } from 'src/environments/environment';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
@@ -15,20 +15,21 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
     AppRoutingModule,
     AppLayoutModule,
     HttpClientModule,
+    BrowserAnimationsModule,
     AuthModule.forRoot({
-        domain: environment.auth.domain,
-        clientId: environment.auth.clientId,
-        authorizationParams: {
-          audience: environment.auth.authorizationParams.audience,
-          redirect_uri: environment.auth.authorizationParams.redirect_uri,
-        }})
-],
+      domain: environment.auth.domain,
+      clientId: environment.auth.clientId,
+      authorizationParams: {
+        audience: environment.auth.authorizationParams.audience,
+        redirect_uri: environment.auth.authorizationParams.redirect_uri,
+      },
+    }),
+  ],
   providers: [
     {
       provide: LocationStrategy,
       useClass: PathLocationStrategy,
-
-    }
+    },
   ],
   bootstrap: [AppComponent],
 })
