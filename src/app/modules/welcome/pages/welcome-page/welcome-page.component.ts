16403550996a174bc6661820bd81a22ee0aa1495
Ignacio <ignaciosalazar986@gmail.com>
2024-09-17 18:37:54 -0300

    animacion

--- src/app/modules/welcome/pages/welcome-page/welcome-page.component.ts
+++ src/app/modules/welcome/pages/welcome-page/welcome-page.component.ts
@@ -1,14 +1,85 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import {
+  trigger,
+  style,
+  transition,
+  animate,
+  state,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-welcome-page',
   templateUrl: './welcome-page.component.html',
   styleUrl: './welcome-page.component.scss',
+  animations: [
+    trigger('expandState1', [
+      state(
+        'collapsed',
+        style({
+          display: 'none',
+        })
+      ),
+      state(
+        'expanded',
+        style({
+          width: '900px',
+        })
+      ),
+      transition('collapsed => expanded', [animate('300ms ease-out')]),
+      transition('expanded => collapsed', [animate('300ms ease-in')]),
+    ]),
+    trigger('expandState2', [
+      state(
+        'collapsed',
+        style({
+          display: 'none',
+        })
+      ),
+      state(
+        'expanded',
+        style({
+          width: '900px',
+        })
+      ),
+      transition('collapsed => expanded', [animate('300ms ease-out')]),
+      transition('expanded => collapsed', [animate('300ms ease-in')]),
+    ]),
+    trigger('expandState3', [
+      state(
+        'collapsed',
+        style({
+          display: 'none',
+        })
+      ),
+      state(
+        'expanded',
+        style({
+          width: '900px',
+        })
+      ),
+      transition('collapsed => expanded', [animate('300ms ease-out')]),
+      transition('expanded => collapsed', [animate('300ms ease-in')]),
+    ]),
+  ],
 })
 export class WelcomePageComponent {
   constructor(private router: Router) {}
+  isExpanded1 = false;
+  isExpanded2 = false;
+  isExpanded3 = false;
 
+  toggleExpand1() {
+    this.isExpanded1 = !this.isExpanded1;
+  }
+
+  toggleExpand2() {
+    this.isExpanded2 = !this.isExpanded2;
+  }
+
+  toggleExpand3() {
+    this.isExpanded3 = !this.isExpanded3;
+  }
   goToAll() {
     this.router.navigate(['/all']);
   }
