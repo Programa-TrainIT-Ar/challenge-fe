5aea66736d2ccb87fbc68aff0195ee284f0b1549
matia <matiasminig@gmail.com>
2024-09-16 21:45:22 -0300

    actualizando components

--- src/app/modules/new/pages/new-pages/new-pages.component.ts
+++ src/app/modules/new/pages/new-pages/new-pages.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-new-pages',
-  templateUrl: './new-pages.component.html',
-  styleUrl: './new-pages.component.scss'
+    selector: 'app-new-pages',
+    templateUrl: './new-pages.component.html',
+    styleUrl: './new-pages.component.scss'
 })
 export class NewPagesComponent {
+
 
 }
