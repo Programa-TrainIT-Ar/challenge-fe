127dacafea59e4f8cd7fee07c4373d7840cc6204
Agustindardanelli-stack <agusdardanelli16@gmail.com>
2024-09-18 00:43:24 -0300

    Component form

--- src/app/modules/new/new.module.ts
+++ src/app/modules/new/new.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NewPagesComponent } from './pages/new-pages/new-pages.component';
 import { NewRoutingModule } from './new-routing.module';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [NewPagesComponent],
-  imports: [CommonModule, NewRoutingModule],
+  imports: [CommonModule, NewRoutingModule,FormsModule],
   exports: [NewPagesComponent],
 })
 export class NewModule {}
--- src/app/modules/new/pages/new-pages/new-pages.component.ts
+++ src/app/modules/new/pages/new-pages/new-pages.component.ts
@@ -7,5 +7,24 @@ import { Component } from '@angular/core';
 })
 export class NewPagesComponent {
 
+    questionTypes: string[] = ['Selección mutiple', 'Casilla', 'Verdadero o falso'];
+  options: string[] = ['Opción 1'];
+  selectedOption: string = '';
 
+  // Agregar una nueva opción
+  addOption() {
+    this.options.push(`Opción ${this.options.length + 1}`);
+  }
+
+  // Acción al enviar el formulario
+  onSubmit(form: any) {
+    console.log('Formulario enviado', form.value);
+  }
+
+  // Crear una nueva pregunta
+  addNewQuestion(form:any) {
+    console.log('Nueva pregunta agregada');
+    form.resetForm();
+    this.options = ['Opción 1'];  // Reiniciar las opciones
+  }
 }
