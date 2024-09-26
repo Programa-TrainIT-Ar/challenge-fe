00f200b6891c38dc37f7f88f612645a4ce5df028
ignacio salazar <ignaciosalazar986@gmail.com>
2024-09-26 19:24:52 -0300

    validaciones

--- src/app/modules/new/pages/new-pages/new-pages.component.ts
+++ src/app/modules/new/pages/new-pages/new-pages.component.ts
@@ -8,6 +8,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class NewPagesComponent {
   public selectHeaderForm: FormGroup;
+  public titleForm: FormGroup;
   constructor(private formsBuilder: FormBuilder) {}
 
   ngOnInit(): void {
@@ -16,6 +17,10 @@ export class NewPagesComponent {
       celula: ['', Validators.required],
       level: ['', Validators.required],
     });
+    this.titleForm = this.formsBuilder.group({
+      nombreQuiz :[''],
+      descripcion: [''],
+    })
   }
 
   questionTypes: string[] = [
@@ -23,6 +28,8 @@ export class NewPagesComponent {
     'Casilla',
     'Verdadero o falso',
   ];
+
+  
 
   options: string[] = [];
   selection: string[] = [''];
@@ -34,10 +41,11 @@ export class NewPagesComponent {
   showSubmits: boolean = false;
   inputType: string = '';
 
-  // DETECTA EL CAMBIO DE PREGUNTA
   onQuestionTypeChange(selectedType: string) {
-    this.array = []; /* <--- este array se crea porque no permite hacer push a selection directamente */
-    this.selection = []; /* <--- 'DEBERIA' limpiar el array, pero en modo 'CASILLA' no se limpia */
+    this.array =
+      []; /* <--- este array se crea porque no permite hacer push a selection directamente */
+    this.selection =
+      []; /* <--- 'DEBERIA' limpiar el array, pero en modo 'CASILLA' no se limpia */
     if (selectedType === 'Verdadero o falso') {
       this.isTrueFalseQuestion = true;
       this.options = ['Verdadero', 'Falso'];
@@ -53,25 +61,28 @@ export class NewPagesComponent {
     } else if (selectedType == 'Casilla') {
       this.isTrueFalseQuestion = false;
       this.showPlus = false;
-      this.options = ['Opción 1.', 'Opción 2.']; /* haciendo distinto el valor funciona */
+      this.options = [
+        'Opción 1.',
+        'Opción 2.',
+      ]; /* haciendo distinto el valor funciona */
       this.showSubmits = true;
       this.inputType = 'radio';
     }
   }
 
-    answerChoice(i: number, type: string, form: any) {
+  answerChoice(i: number, type: string, form: any) {
     if (type === 'radio') {
-      this.array = []; /* <--- este array se crea porque no permite hacer push a selection directamente */
-      this.selection = []; /* <--- 'DEBERIA' limpiar el array, pero en modo 'CASILLA' no se limpia */
+      this.array =
+        []; /* <--- este array se crea porque no permite hacer push a selection directamente */
+      this.selection =
+        []; /* <--- 'DEBERIA' limpiar el array, pero en modo 'CASILLA' no se limpia */
       this.array.push(this.options[i]);
       this.selection = this.array;
       console.log(this.selection);
-      
     } else if (this.array.includes(this.options[i])) {
       this.array = this.array.filter(el => el != this.options[i]);
       this.selection = this.array;
       console.log(this.array);
-
     } else {
       this.array = this.array.filter(el => el != 'Verdadero');
       this.array = this.array.filter(el => el != 'Falso');
@@ -90,9 +101,12 @@ export class NewPagesComponent {
 
   // enviar el formulario
   onSubmit(form: any) {
-    const formHeader = this.selectHeaderForm.valid;
+    const formHeader = this.selectHeaderForm.value;
     const formSection = form.value;
     console.log(formSection);
+    console.log(formHeader);
+    console.log(this.titleForm.value);
+    
   }
   // Crear una nueva pregunta
   addNewQuestion(form: any) {
