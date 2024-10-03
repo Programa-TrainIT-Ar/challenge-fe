49774ffeb754d02f93a025b8e2e38394b8c67a30
Agustindardanelli-stack <agusdardanelli16@gmail.com>
2024-10-03 20:42:39 -0300

    adjusting container layaout

--- src/app/modules/all/all.module.ts
+++ src/app/modules/all/all.module.ts
@@ -3,10 +3,10 @@ import { CommonModule } from '@angular/common';
 import { AllPageComponent } from './pages/all-page/all-page.component';
 import { AllRoutingModule } from './all-routing.module';
 import { FormsModule } from '@angular/forms';
-
+import { HeaderFormModule } from 'src/app/shared/components/header-form/header-form.module';
 @NgModule({
   declarations: [AllPageComponent],
-  imports: [CommonModule, AllRoutingModule, FormsModule],
+  imports: [CommonModule, AllRoutingModule, FormsModule, HeaderFormModule],
   exports: [AllPageComponent]
 })
 export class AllModule {}
