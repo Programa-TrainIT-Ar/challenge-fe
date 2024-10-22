82daae8031854f0dd51dd41a0bec59198cbb1bc1
JavierBrizuela <jbrizuela1976@gmail.com>
2024-10-21 21:48:12 -0300

    add enviroment production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
   production: true,
+  auth: {
+    domain: '${process.env.DOMAIN}',
+    clientId: '${process.env.CLIENT_ID}',
+    authorizationParams: {
+      audience: '${process.env.AUDIENCE}',
+      redirect_uri: '${process.env.REDIRECT_URL}',
+    },
+  },
+  url: '${process.env.URL}'
 };
