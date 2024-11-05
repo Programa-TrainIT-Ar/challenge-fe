0fd5cc5b940aae07c8492e70eb4fb198e5534978
Ignacio <ignaciosalazar986@gmail.com>
2024-11-05 18:51:13 -0300

    conflictos

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SideBarComponent } from './components/sideBar/side-bar/side-bar.component';
-import {PopMessageComponent} from './components/pop-message/pop-message.component'
+import { PopMessageComponent } from './components/pop-message/pop-message.component';
 
 @NgModule({
   declarations: [SideBarComponent, PopMessageComponent],
