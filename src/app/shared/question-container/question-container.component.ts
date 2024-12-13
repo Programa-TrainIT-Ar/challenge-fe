73514f5d652c720ca541d39dc047dd5eeee14819
JavierBrizuela <jbrizuela1976@gmail.com>
2024-12-13 19:57:28 -0300

    change quiz id

--- src/app/shared/question-container/question-container.component.ts
+++ src/app/shared/question-container/question-container.component.ts
@@ -61,7 +61,7 @@ export class QuestionContainerComponent implements OnInit{
       }));
       this.updateQuestion();
     } else {
-      this.questionService.getQuiz('f34640ec-4a11-4bda-9b0c-5b32ff6f1aec').subscribe((response:any)=>{
+      this.questionService.getQuiz('710fa901-b6ac-4623-b8f6-23c76131584c').subscribe((response:any)=>{
         this.questions = response.questions
         if (this.questions.length > 0){
           this.updateQuestion();
