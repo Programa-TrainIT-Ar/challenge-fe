f7f0fb6eda09b51857f144391c3733b805c89276
JavierBrizuela <jbrizuela1976@gmail.com>
2025-07-04 21:59:15 -0300

    fix redirect_uri

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -5,7 +5,7 @@ export const environment = {
       clientId: "TTVpH09MQpHbU6oHQjXAVt4I66h9f2Hk",
       authorizationParams: {
         audience: 'https://challenge-be-development-99e1.onrender.com',
-        redirect_uri: 'https://challenge-fe-development.netlify.app/candidato',
+        redirect_uri: 'https://challenge-fe-development.netlify.app/candidato/',
       },
     },
     url: 'https://challenge-be-development-99e1.onrender.com'
