9c147c017051dddcfa0592a97c6d63eb88978986
JavierBrizuela <jbrizuela1976@gmail.com>
2025-07-04 21:47:13 -0300

    fix redirect_uri

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
     auth: {
-      domain: "dev-etnwb5jjlx4qpvdm.us.auth0.com",
-      clientId: "cDXMB8lOxTHRJUodDMCLLrbxbooFe2le",
+      domain: "dev-hwbroc2d1yqqr04v.us.auth0.com",
+      clientId: "TTVpH09MQpHbU6oHQjXAVt4I66h9f2Hk",
       authorizationParams: {
-        audience: 'https://api.miaplicacion.com',
-        redirect_uri: 'https://challenge-fe-development.netlify.app',
+        audience: 'https://challenge-be-development-99e1.onrender.com',
+        redirect_uri: 'https://challenge-fe-development.netlify.app/candidato',
       },
     },
     url: 'https://challenge-be-development-99e1.onrender.com'
