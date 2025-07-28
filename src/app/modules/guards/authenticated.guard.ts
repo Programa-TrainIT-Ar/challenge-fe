14b4e794aa078d9fd614eba9b0b804aeb9c9e3bf
JavierBrizuela <jbrizuela1976@gmail.com>
2025-07-28 16:11:59 -0300

    fix: authentication-guard

--- src/app/modules/guards/authenticated.guard.ts
+++ src/app/modules/guards/authenticated.guard.ts
@@ -1,7 +1,7 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '@auth0/auth0-angular';
-import { switchMap, tap. map } from 'rxjs';
+import { switchMap, tap, map } from 'rxjs';
 
 export const authenticatedGuard: CanActivateFn = (route, state) => {
   const auth = inject(AuthService);
