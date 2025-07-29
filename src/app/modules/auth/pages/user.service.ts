cdb462ced4858786f525d2908d7e3aa1f94be45c
Sanrvallejo <zanti-23@hotmail.com>
2025-07-29 18:55:04 -0500

    feature: user & sign-up services fixed

--- src/app/modules/auth/pages/user.service.ts
+++ src/app/modules/auth/pages/user.service.ts
@@ -32,25 +32,7 @@ export class UserService {
   }
 
   sendResetLink(email: string) {
-    //descomentar la siguiente línea para que funcione la solicitud
-    // return this.http.post(`${this.urlApi}/forgot-password`, { email });
-
-    //borrar lo siguiente
-    const simulateSuccess = true;
-    if (simulateSuccess) {
-      console.log('SIMULACIÓN: Registro exitoso para datos:', email);
-      return of({
-        message:
-          'Usuario registrado exitosamente. Correo de confirmación enviado.',
-        userId: 'mock-user-123',
-        email: (email as any).email,
-      }).pipe(delay(1000));
-    } else {
-      console.error('SIMULACIÓN: Error en el registro para datos:', email);
-      return throwError(
-        () => new Error('Simulación: Error al procesar el registro.')
-      );
-    }
+    return this.http.post(`${this.urlApi}/forgot-password`, { email });
   }
 
   registerUser(userData: UserData) {
--- src/app/modules/candidato-sign-up/pages/sign-up.service.ts
+++ src/app/modules/candidato-sign-up/pages/sign-up.service.ts
@@ -13,24 +13,6 @@ export class SignUpService {
   constructor() {}
 
   registerUser(data: Object) {
-    //descomentar la siguiente línea para que vuelva a funcionar la solicitud
-    // return this.http.post(this.url, data);
-
-    //eliminar lo siguiente
-    const simulateSuccess = true;
-    if (simulateSuccess) {
-      console.log('SIMULACIÓN: Registro exitoso para datos:', data);
-      return of({
-        message:
-          'Usuario registrado exitosamente. Correo de confirmación enviado.',
-        userId: 'mock-user-123',
-        email: (data as any).email,
-      }).pipe(delay(1000));
-    } else {
-      console.error('SIMULACIÓN: Error en el registro para datos:', data);
-      return throwError(
-        () => new Error('Simulación: Error al procesar el registro.')
-      );
-    }
+    return this.http.post(this.urlApi, data);
   }
 }
