432bc2460109fc42f90cefcba934cd229578a0b5
JavierBrizuela <jbrizuela1976@gmail.com>
2025-07-31 00:29:40 -0300

    fix: resolve conflict in pull

--- src/app/modules/auth/pages/user.service.ts
+++ src/app/modules/auth/pages/user.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
-import { delay, of, throwError } from 'rxjs';
 
 export interface UserData {
   email: string;
