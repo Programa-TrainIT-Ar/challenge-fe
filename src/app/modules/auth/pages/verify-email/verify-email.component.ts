3b869ddb6c00af2307a60c79d9245e275ac798e5
Huron <charlie.walls97@gmail.com>
2025-08-18 20:14:54 -0500

    feat: add modals to verify-token

--- src/app/modules/auth/pages/verify-email/verify-email.component.ts
+++ src/app/modules/auth/pages/verify-email/verify-email.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
 import { UserService } from '../user.service';
 
 @Component({
@@ -10,50 +9,58 @@ import { UserService } from '../user.service';
 })
 export class VerifyEmailComponent implements OnInit {
   loading = true;
-  errorMessage = '';
+
+  showModal = false;
+  currentModal: 'expired' | 'invalid' | 'notConfirmed' | 'error' | null = null;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private http: HttpClient,
     private userService: UserService
   ) {}
 
   ngOnInit(): void {
-    console.log('VerifyEmailComponent ngOnInit');
     this.route.queryParams.subscribe(params => {
       const token = params['token'];
       if (!token) {
-        this.errorMessage = 'Token no proporcionado';
+        this.showModal = true;
+        this.currentModal = 'invalid';
         this.loading = false;
         return;
       }
 
-      // Confirmar el email con el token
       this.userService.VerifyEmail(token).subscribe({
         next: (res: any) => {
           this.loading = false;
-          // Validamos que el email esté confirmado
           if (res.emailConfirmed === true) {
-            const id = res.id;
+            const id = res.user_id;
             const email = res.email;
-            const name = res.first_name || res.name || '';
+            const name = res.name || '';
             this.router.navigate(['/account-setup'], {
               queryParams: { email, name, id },
             });
           } else {
-            this.errorMessage = 'El email no ha sido confirmado aún.';
+            this.showModal = true;
+            this.currentModal = 'notConfirmed';
           }
         },
         error: error => {
           this.loading = false;
           if (error.status === 400) {
-            this.errorMessage = 'El enlace ha expirado o es inválido.';
+            this.showModal = true;
+            this.currentModal = 'expired';
           } else {
-            this.errorMessage = 'Error inesperado al verificar el token.';
+            this.showModal = true;
+            this.currentModal = 'error';
           }
         },
       });
     });
+  }
+
+  closeModal() {
+    this.showModal = false;
+    this.currentModal = null;
+    this.router.navigate(['/register']); // o donde quieras que vaya si falla
   }
 }
