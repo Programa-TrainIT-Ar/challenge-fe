27b5b24b424639a0272b3631faee722c2dd47d43
JSolorzano <jsolorzanosc@gmail.com>
2025-08-29 21:49:10 -0400

    fix: habilitando nombre de usuario y fecha en el dashboard

--- src/app/modules/candidato-dashboard/dashboard-page/dashboard-page.component.ts
+++ src/app/modules/candidato-dashboard/dashboard-page/dashboard-page.component.ts
@@ -1,22 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '@auth0/auth0-angular';
+import { AuthService, User } from '@auth0/auth0-angular';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
-  styleUrls: ['./dashboard-page.component.scss']
+  styleUrls: ['./dashboard-page.component.scss'],
 })
 export class DashboardPageComponent implements OnInit {
-  user$ = this.auth.user$
+  user$: Observable<User | null> = this.auth.user$;
   userName: string = 'Usuario'; // Puedes obtener el nombre de usuario desde algún servicio
-  currentDate: string = new Date().toLocaleDateString(); // Fecha actual
+  currentDate: Date = new Date(); // Fecha actual
   items: any[] = [
     { title: 'Diseño UX', icon: 'assets/ui.png' },
     { title: 'Frontend', icon: 'assets/frontend.png' },
     { title: 'Backend', icon: 'assets/backend.png' },
     { title: 'Fullstack', icon: 'assets/fullstack.png' },
     { title: 'PM', icon: 'assets/qa.png' },
-    { title: 'QA', icon: 'assets/scrum.png' }
+    { title: 'QA', icon: 'assets/scrum.png' },
   ];
 
   constructor(private auth: AuthService) {}
