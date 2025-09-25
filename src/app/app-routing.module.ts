5b40ff2af44a79f195068af9255edecf36fb8745
MaxiAramayo <maxiaramayolazo@hotmail.com>
2025-09-24 22:18:11 -0300

    fix/ se agrega el path de quiz/id

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,7 @@ import { ErrorComponent } from './modules/home/pages/error/error.component';
 
 import { VerifyEmailComponent } from './modules/auth/pages/verify-email/verify-email.component';
 import { CandidatoFormComponent } from './modules/candidato-dashboard/candidato-form/candidato-form.component';
+import { QuizTakeComponent } from './modules/quiz/pages/quiz-take/quiz-take.component';
 @NgModule({
   imports: [
     RouterModule.forRoot(
@@ -44,6 +45,12 @@ import { CandidatoFormComponent } from './modules/candidato-dashboard/candidato-
           canActivate: [AuthGuard],
           loadChildren: () =>
             import('./modules/quiz/quiz.module').then(m => m.QuizModule),
+        },
+        {
+          path: 'quiz/:id',
+          canActivate: [AuthGuard],
+          loadChildren: () =>
+            import('./modules/quiz/pages/quiz-take/quiz-take.component').then(m => m.QuizTakeComponent),
         },
         {
           path: 'dashboard',
