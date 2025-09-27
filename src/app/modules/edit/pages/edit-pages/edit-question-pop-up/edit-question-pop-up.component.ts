756724044e3737df3756364df2c84cf0666c39a7
juan <bravo43juancho@gmail.com>
2025-09-27 11:13:49 -0300

    FIX: no se muestra el tipo de pregunta al editar una.

--- src/app/modules/edit/pages/edit-pages/edit-question-pop-up/edit-question-pop-up.component.ts
+++ src/app/modules/edit/pages/edit-pages/edit-question-pop-up/edit-question-pop-up.component.ts
@@ -2,11 +2,12 @@ import { Component, EventEmitter, Input,  Output } from '@angular/core';
 import {   FormsModule } from '@angular/forms';
 import { CommonModule   } from '@angular/common';
 import { Question } from '../../../../../shared/question-container/question-interface';
+import { TypeOptionsPipe } from '../type-options.pipe';
 
 @Component({
   selector: 'app-edit-question-pop-up',
   standalone: true,
-  imports: [FormsModule ,CommonModule],
+  imports: [FormsModule, CommonModule, TypeOptionsPipe],
   templateUrl: './edit-question-pop-up.component.html',
   styleUrl: './edit-question-pop-up.component.scss',
 })
@@ -23,28 +24,28 @@ export class EditQuestionPopUpComponent {
   @Output() cancel = new EventEmitter<void>();
   showSubmits: boolean = true;
   questionTypes: string[] = [
-      'Selección mutiple',
-      'Casilla',
-      'Verdadero o falso',
+    'multiple_choice',
+    'simple_choice',
+    'true_false',
   ];
 
   saveChanges() {
-      this.editedQuestion.emit(this.currentEditingQuestion);
+    this.editedQuestion.emit(this.currentEditingQuestion);
   }
   closeEditPopup() {
-      this.cancel.emit();
+    this.cancel.emit();
   }
-  addOption(){
+  addOption() {
     this.currentEditingQuestion.options.push('Opción nueva');
   }
-  private changeType() : string{
-    if (this.selectedOption == 'Selección mutiple') return 'multiple_choice'
-    if (this.selectedOption == 'Casilla') return 'simple_choice'
-    if (this.selectedOption == 'Verdadero o falso') return 'true_false'
-    return ''
+  private changeType(): string {
+    if (this.selectedOption == 'Selección mutiple') return 'multiple_choice';
+    if (this.selectedOption == 'Casilla') return 'simple_choice';
+    if (this.selectedOption == 'Verdadero o falso') return 'true_false';
+    return '';
   }
 
-  answerChoice(index: number){
+  answerChoice(index: number) {
     const idx = this.currentEditingQuestion.correct_option.indexOf(index);
     if (idx > -1) {
       this.currentEditingQuestion.correct_option.splice(idx, 1);
@@ -52,7 +53,7 @@ export class EditQuestionPopUpComponent {
       this.currentEditingQuestion.correct_option.push(index);
     }
   }
-  changeText($event, index){
+  changeText($event, index) {
     this.currentEditingQuestion.options[index] = $event.target.value;
   }
   onQuestionTypeChange() {
@@ -62,12 +63,16 @@ export class EditQuestionPopUpComponent {
     this.showSubmits = true;
     const selectedType = this.changeType();
     if (selectedType === 'true_false') {
-      this.currentEditingQuestion.options = ["Verdadero", "Falso"];
+      this.currentEditingQuestion.options = ['Verdadero', 'Falso'];
       this.inputType = 'radio';
       this.isTrueFalseQuestion = true;
       this.showPlus = false;
     } else if (selectedType === 'multiple_choice') {
-      this.currentEditingQuestion.options = ['Opción 1', 'Opción 2', 'Opción 3'];
+      this.currentEditingQuestion.options = [
+        'Opción 1',
+        'Opción 2',
+        'Opción 3',
+      ];
       this.inputType = 'checkbox';
       this.isTrueFalseQuestion = false;
       this.showPlus = true;
---
+++ src/app/modules/edit/pages/edit-pages/type-options.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { TypeOptionsPipe } from './type-options.pipe';
+
+describe('TypeOptionsPipe', () => {
+  it('create an instance', () => {
+    const pipe = new TypeOptionsPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
